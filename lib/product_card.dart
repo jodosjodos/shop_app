1d56e850d1b3a4b1841b3b6049c4240f7313ce52
jodosjodos <jeandedieu2030@gmail.com>
2024-04-29 16:21:54 +0200

    [add]  fix  margin problems

--- lib/product_card.dart
+++ lib/product_card.dart
@@ -12,34 +12,33 @@ class ProductCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      margin: const EdgeInsets.all(20),
+      padding: const EdgeInsets.all(16),
       color: const Color.fromRGBO(216, 230, 253, 1),
-      child: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              title,
-              style: Theme.of(context).textTheme.titleMedium,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            title,
+            style: Theme.of(context).textTheme.titleMedium,
+          ),
+          const SizedBox(
+            height: 5,
+          ),
+          Text(
+            "\$$price",
+            style: Theme.of(context).textTheme.bodySmall,
+          ),
+          const SizedBox(
+            height: 5,
+          ),
+          Center(
+            child: Image.asset(
+              image,
+              height: 175,
             ),
-            const SizedBox(
-              height: 5,
-            ),
-            Text(
-              "\$$price",
-              style: Theme.of(context).textTheme.bodySmall,
-            ),
-            const SizedBox(
-              height: 5,
-            ),
-            Center(
-              child: Image.asset(
-                image,
-                height: 175,
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
