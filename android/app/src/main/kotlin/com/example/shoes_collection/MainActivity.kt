6fa8092a42c7971323c01a5b79088600f2d4859e
jodosjodos <jeandedieu2030@gmail.com>
2024-04-29 11:02:48 +0200

    [init] init shop collection

---
+++ android/app/src/main/kotlin/com/example/shoes_collection/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.shoes_collection
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:shoes_collection/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Shopping App",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        fontFamily: "Lato",
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color.fromRGBO(254, 206, 1, 1),
+          primary: const Color.fromRGBO(254, 206, 1, 1),
+        ),
+        inputDecorationTheme: const InputDecorationTheme(
+            hintStyle: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+            prefixIconColor: Color.fromRGBO(119, 119, 119, 1)),
+      ),
+      home: const HomePage(),
+    );
+  }
+}
