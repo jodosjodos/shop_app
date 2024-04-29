861b9319da05492aa4288542d128ec878ffd4733
jodosjodos <jeandedieu2030@gmail.com>
2024-04-29 18:01:34 +0200

    [fix]  done with details page

--- lib/product_details_page.dart
+++ lib/product_details_page.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class ProductDetailsPage extends StatelessWidget {
   const ProductDetailsPage({
@@ -19,6 +21,77 @@ class ProductDetailsPage extends StatelessWidget {
             product["title"] as String,
             style: Theme.of(context).textTheme.titleLarge,
           ),
+          const Spacer(),
+          Padding(
+            padding: const EdgeInsets.all(16),
+            child: Image.asset(
+              product["imageUrl"] as String,
+            ),
+          ),
+          const Spacer(
+            flex: 2,
+          ),
+          Container(
+            height: 250,
+            decoration: BoxDecoration(
+              color: const Color.fromRGBO(245, 247, 249, 1),
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "\$${product["price"]}",
+                  style: Theme.of(context).textTheme.titleLarge,
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                SizedBox(
+                  height: 50,
+                  child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: (product["sizes"] as List<int>).length,
+                    itemBuilder: (context, index) {
+                      final size = (product["sizes"] as List<int>)[index];
+                      return Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Chip(
+                          label: Text(
+                            size.toString(),
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: ElevatedButton.icon(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Theme.of(context).colorScheme.primary,
+                      minimumSize: const Size(
+                        double.infinity,
+                        50,
+                      ),
+                    ),
+                    icon: const Icon(
+                      Icons.shopping_cart_outlined,
+                      color: Colors.black,
+                    ),
+                    label: const Text(
+                      "Add To Cart",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          )
         ],
       ),
     );
